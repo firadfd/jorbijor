3892a1e73de6e7dc5d7169f35669f8635097a13f
Firad Fd <firadfd833@gmail.com>
2025-09-07 20:39:50 +0600

    version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.jorbijor.com"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "6.0"
+        versionCode = 10
+        versionName = "10.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
